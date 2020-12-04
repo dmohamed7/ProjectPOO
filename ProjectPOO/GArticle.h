22fbd41d9450eebd81ba815fd3a2f0d02f82fa42
Mehdi GUERRIB <mehdi.guerrib.dz@viacesi.fr>
2020-12-04 12:40:34 +0100

    push mehdi7

--- ProjectPOO/GArticle.h
+++ ProjectPOO/GArticle.h
@@ -38,6 +38,13 @@ namespace ProjectPOO {
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Label^ label7;
 	protected:
 
 
@@ -67,6 +74,13 @@ namespace ProjectPOO {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
 			// 
 			// button1
@@ -130,6 +144,77 @@ namespace ProjectPOO {
 			this->button4->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
 			this->button4->UseVisualStyleBackColor = false;
 			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::Transparent;
+			this->label1->Location = System::Drawing::Point(35, 78);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(77, 17);
+			this->label1->TabIndex = 5;
+			this->label1->Text = L"reference :";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::Color::Transparent;
+			this->label2->Location = System::Drawing::Point(682, 47);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(21, 17);
+			this->label2->TabIndex = 6;
+			this->label2->Text = L"ID";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::Color::Transparent;
+			this->label3->Location = System::Drawing::Point(35, 320);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(61, 17);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"prix TH :";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::Color::Transparent;
+			this->label4->Location = System::Drawing::Point(35, 269);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(70, 17);
+			this->label4->TabIndex = 8;
+			this->label4->Text = L"Quantité :";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->BackColor = System::Drawing::Color::Transparent;
+			this->label5->Location = System::Drawing::Point(35, 218);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(78, 17);
+			this->label5->TabIndex = 9;
+			this->label5->Text = L"Taux TVA :";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->BackColor = System::Drawing::Color::Transparent;
+			this->label6->Location = System::Drawing::Point(35, 171);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(65, 17);
+			this->label6->TabIndex = 10;
+			this->label6->Text = L"Couleur :";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->BackColor = System::Drawing::Color::Transparent;
+			this->label7->Location = System::Drawing::Point(35, 124);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(91, 17);
+			this->label7->TabIndex = 11;
+			this->label7->Text = L"Désignation :";
+			this->label7->Click += gcnew System::EventHandler(this, &GArticle::label7_Click);
+			// 
 			// GArticle
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
@@ -138,6 +223,13 @@ namespace ProjectPOO {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(907, 509);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
 			this->Controls->Add(this->button4);
 			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
@@ -147,8 +239,11 @@ namespace ProjectPOO {
 			this->RightToLeft = System::Windows::Forms::RightToLeft::No;
 			this->Text = L"GArticle";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void label7_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+};
 }
