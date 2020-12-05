8216e8f837734abdbf4b8dc41b1d8a6bb29ae9fd
Mehdi GUERRIB <mehdi.guerrib.dz@viacesi.fr>
2020-12-05 21:06:48 +0100

    Revert "Revert "pyshfacture""
    This reverts commit 9f998ab5dbde029be352ee90fc70d3d8b5708e4e.

--- ProjectPOO/facture.h
+++ ProjectPOO/facture.h
@@ -1,5 +1,93 @@
-#pragma once
-ref class facture
-{
-};
+#include "Ccommande.h"
+#include "Client.h"
+//include "Adresse.h"
+//include "Paiement.h"
 
+
+namespace NS_Composants {
+	using namespace System;
+	ref class facture
+	{
+
+	private:
+
+		int ID_Facture;
+
+		String^ Date_Facturation;
+
+		float Montant_Total_HT;
+
+		float Montant_Total_de_TVA;
+
+		float Montant_Total_TTC;
+
+		String^ Nom_société;
+
+		String^ Add_société;
+
+		int Num_service_client;
+
+		int ID_commande;
+
+		int ID_client;
+
+		int ID_paiement;
+
+		int ID_Adresse;
+
+	public:
+
+		// les setter et les getter
+
+		int getID_Facture();
+
+		String^ getDate_Facturation();
+
+		float getMontant_Total_HT();
+
+		float getMontant_Total_de_TVA();
+
+		float getMontant_Total_TTC();
+
+		String^ getNom_société();
+
+		String^ getAdd_société();
+
+		int getNum_service_client();
+
+		int getID_commande();
+
+		int getID_client();
+
+		int getID_paiement();
+
+		int getID_Adresse();
+
+		void setID_Facture(int);
+
+		void setDate_Facturation(String^);
+
+		void setMontant_Total_HT(float);
+
+		void setMontant_Total_de_TVA(float);
+
+		void setMontant_Total_TTC(float);
+
+		void setNom_société(String^);
+
+		void setAdd_société(String^);
+
+		void setNum_service_client(int);
+
+		void setID_commande(int);
+
+		void setID_client(int);
+
+		void setID_paiement(int);
+
+		void setID_Adresse(int);
+
+		facture();
+
+	};
+}
