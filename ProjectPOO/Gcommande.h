8a2f31d1bf5e9f26685d999f079dc178eceacb09
Mehdi GUERRIB <mehdi.guerrib.dz@viacesi.fr>
2020-12-05 19:05:12 +0100

    designcmd

--- ProjectPOO/Gcommande.h
+++ ProjectPOO/Gcommande.h
@@ -36,12 +36,16 @@ namespace ProjectPOO {
 		}
 	private: System::Windows::Forms::Button^ button1;
 	protected:
+	private: System::ComponentModel::BackgroundWorker^ backgroundWorker1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button4;
 
 	private:
 		/// <summary>
 		/// Variable nécessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -52,25 +56,85 @@ namespace ProjectPOO {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Gcommande::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->backgroundWorker1 = (gcnew System::ComponentModel::BackgroundWorker());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(110, 387);
+			this->button1->BackColor = System::Drawing::Color::Transparent;
+			this->button1->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Center;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.Image")));
+			this->button1->ImageAlign = System::Drawing::ContentAlignment::TopCenter;
+			this->button1->Location = System::Drawing::Point(242, 249);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(143, 58);
+			this->button1->Size = System::Drawing::Size(86, 75);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &Gcommande::button1_Click);
+			this->button1->Text = L"Ajouter";
+			this->button1->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
+			this->button1->UseVisualStyleBackColor = false;
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Transparent;
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button2.Image")));
+			this->button2->ImageAlign = System::Drawing::ContentAlignment::TopCenter;
+			this->button2->Location = System::Drawing::Point(282, 369);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(86, 74);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L"Modifier";
+			this->button2->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
+			this->button2->UseVisualStyleBackColor = false;
+			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::Transparent;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.Image")));
+			this->button3->ImageAlign = System::Drawing::ContentAlignment::TopCenter;
+			this->button3->Location = System::Drawing::Point(625, 369);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(86, 74);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"Supprimer";
+			this->button3->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
+			this->button3->UseVisualStyleBackColor = false;
+			// 
+			// button4
+			// 
+			this->button4->BackColor = System::Drawing::Color::Transparent;
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button4.Image")));
+			this->button4->ImageAlign = System::Drawing::ContentAlignment::TopCenter;
+			this->button4->Location = System::Drawing::Point(737, 262);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(86, 75);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"Afficher";
+			this->button4->TextAlign = System::Drawing::ContentAlignment::BottomCenter;
+			this->button4->UseVisualStyleBackColor = false;
+			this->button4->Click += gcnew System::EventHandler(this, &Gcommande::button4_Click);
 			// 
 			// Gcommande
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::Control;
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
-			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Zoom;
-			this->ClientSize = System::Drawing::Size(907, 509);
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(1049, 524);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->DoubleBuffered = true;
 			this->Name = L"Gcommande";
@@ -79,9 +143,7 @@ namespace ProjectPOO {
 
 		}
 #pragma endregion
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		
-		
+	private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
 	};
 }
